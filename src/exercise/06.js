bab6229ae21b37f3b7714cd34630b9656471bf6d
macosgrove <maryanne@covidence.org>
2023-10-06 14:10:34 +1100

    Exercise 6.2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -10,19 +10,41 @@ import {
 } from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-  if (!pokemonName) return 'Submit a pokemon'
+  if (!pokemonName) return <IdleMessage />
   const [pokemon, setPokemon] = React.useState(null)
   const [error, setError] = React.useState(null)
+  const [status, setStatus] = React.useState('idle')
   React.useEffect(() => {
-    setPokemon(null)
-    setError(null)
+    setStatus('pending')
     fetchPokemon(pokemonName)
       .then(pokemonData => {
+        setStatus('resolved')
         setPokemon(pokemonData)
       })
-      .catch(error => setError(error))
+      .catch(error => {
+        setError(error)
+        setStatus('rejected')
+      })
   }, [pokemonName])
-  if (error) {
+
+  switch (status) {
+    case 'idle':
+      return <IdleMessage />
+    case 'pending':
+      return <PokemonInfoFallback name={pokemonName} />
+    case 'resolved':
+      return <PokemonDataView pokemon={pokemon} />
+    case 'rejected':
+      return <PokemonError />
+    default:
+      return <div>`Invalid status ${status}`</div>
+  }
+
+  function IdleMessage() {
+    return <div>Enter the name of a Pokemon to begin</div>
+  }
+
+  function PokemonError() {
     return (
       <div role="alert">
         There was an error:
@@ -30,8 +52,6 @@ function PokemonInfo({pokemonName}) {
       </div>
     )
   }
-  if (!pokemon) return <PokemonInfoFallback name={pokemonName} />
-  return <PokemonDataView pokemon={pokemon} />
 }
 
 function App() {
